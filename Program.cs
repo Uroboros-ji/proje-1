aa2d46e1f656dae9a7e8b120fc5ed88eda131e74
Uroboros-ji <dimapolschkov@yandex.ru>
2022-02-08 09:58:13 +0300

    test
    test

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//namespace ConsoleApp1
+namespace std
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[]  array = new int[3]; 
+            for ( int j = 1; j < 3; j++)
+            {
+                array[j]  = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(array[j]);
+            }
+            int i = Convert.ToInt32(Console.ReadLine());
+         
+            if (i <= 5) 
+            {
+                Console.WriteLine("Hello World!");
+                Console.ReadLine();
+            }else Console.WriteLine("try agen leter");
+        }
+    }
+}
+//просто здравствуй)))
+/* как
+ * ты 
+ * думаешь
+ * все норм?*/
